60f730f8363d8c275a330310e41cda4cf3c0de77
Koichi Okutsu <okutsu@pej.jp>
2021-02-07 19:52:26 +0900

    [change] exec at am 8:00

--- clock.py
+++ clock.py
@@ -1,11 +1,14 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 import words
+from datetime import datetime
 
 twische = BlockingScheduler()
 
-@twische.scheduled_job('interval',minutes=60*24)
+@twische.scheduled_job('interval',minutes=60)
 def timed_job():
-    words.puttweet()
+    today = datetime.now()
+    if today.hour == 8 :
+        words.puttweet()
 
 if __name__ == "__main__":
     twische.start()

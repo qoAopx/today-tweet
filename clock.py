dc04a7513832052a1dcb02f7e543870b7b7d9013
qoAop <okutsu@pej.jp>
2021-02-25 09:14:29 +0900

    [change] exec freq. down

--- clock.py
+++ clock.py
@@ -4,12 +4,12 @@ from datetime import datetime
 
 twische = BlockingScheduler()
 
-@twische.scheduled_job('interval',minutes=60)
+@twische.scheduled_job('interval',minutes=60*24)
 def timed_job():
     today = datetime.now()
     print(today)
-    if today.hour == 8 :
-        words.puttweet()
+    #if today.hour == 8 :
+    words.puttweet()
 
 if __name__ == "__main__":
     twische.start()
